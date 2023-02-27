1605494fc86aa5e325ca7edd5c93f8a7fb5b8f97
vheh0901@gmail.com <vheh0901@gmail.com>
2023-02-27 17:38:45 +0900

    Feature: "가게명 검색 기능 추가"
    가게명 혹은 가게명에 포함된 단어 검색시 ShopList 변경

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
-import { MapSlice } from "../store/MapSlice";
 import ListItem from "./ListItem";
 
 const List = () => {
   const value = useSelector((state) => state.daegu.value);
   const type = useSelector((state) => state.loca.type);
   const [shopList, setShopList] = useState([]);
+  const [shopName, setShopName] = useState("");
 
   useEffect(() => {
     setShopList(value);
@@ -23,8 +23,32 @@ const List = () => {
     return <div>없음</div>;
   }
 
+  const shopNameValue = (e) => {
+    setTimeout(() => {
+      setShopName(e.target.value);
+    }, 2000);
+  };
+
+  const shopNameSearchClick = (e) => {
+    e.preventDefault();
+    setShopList(
+      value.filter((listItem) => {
+        const itemShopName = listItem.shopName;
+        return itemShopName.includes(shopName);
+      })
+    );
+  };
+
   return (
     <>
+      <form onSubmit={shopNameSearchClick}>
+        <input
+          type="text"
+          onChange={shopNameValue}
+          placeholder="찾고 싶은 가게명"
+        />
+        <input type="submit" value="찾기" />
+      </form>
       <StyledUl>
         {JSON.stringify(shopList) !== "{}" ? (
           shopList.map((item) => {
