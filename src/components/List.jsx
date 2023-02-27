bfe6939648120bde710119c28a07751f09afa203
vheh0901@gmail.com <vheh0901@gmail.com>
2023-02-28 03:00:02 +0900

    Feature: "ShopNameSearch컴포넌트 제작"
    List 컴포넌트 내부에 두니까 코드가 번잡해서져서 따로 컴포넌트를 제작해줌

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
 import ListItem from "./ListItem";
+import ShopNameSearch from "./ShopNameSearch";
 
 const List = () => {
   const value = useSelector((state) => state.daegu.value);
   const type = useSelector((state) => state.loca.type);
   const [shopList, setShopList] = useState([]);
-  const [shopName, setShopName] = useState("");
 
   useEffect(() => {
     setShopList(value);
@@ -23,32 +23,9 @@ const List = () => {
     return <div>없음</div>;
   }
 
-  const shopNameValue = (e) => {
-    setTimeout(() => {
-      setShopName(e.target.value);
-    }, 2000);
-  };
-
-  const shopNameSearchClick = (e) => {
-    e.preventDefault();
-    setShopList(
-      value.filter((listItem) => {
-        const itemShopName = listItem.shopName;
-        return itemShopName.includes(shopName);
-      })
-    );
-  };
-
   return (
     <>
-      <form onSubmit={shopNameSearchClick}>
-        <input
-          type="text"
-          onChange={shopNameValue}
-          placeholder="찾고 싶은 가게명"
-        />
-        <input type="submit" value="찾기" />
-      </form>
+      <ShopNameSearch value={value} setShopList={setShopList} />
       <StyledUl>
         {JSON.stringify(shopList) !== "{}" ? (
           shopList.map((item) => {
